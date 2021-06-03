b601188f5b45a52dd4a419417fccb0557a43bfda
SuperYoplait <60890985+SuperYoplait@users.noreply.github.com>
2021-06-04 05:55:00 +0900

    웹 개선사항
    웹 개선사항

---
+++ kochat_ori/kochat-master/App.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import Navigator from './src/Navigator'
+
+function App() {
+  return (
+    <Navigator/>
+  );
+}
+
+export default App;
