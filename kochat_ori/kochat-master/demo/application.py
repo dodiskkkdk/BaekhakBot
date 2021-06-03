b20fe919c9567d5d9b6ab1b282d834698768e2ee
dodiskkkdk <44862667+dodiskkkdk@users.noreply.github.com>
2021-06-04 06:07:16 +0900

    school
    scenario school

--- kochat_ori/kochat-master/demo/application.py
+++ kochat_ori/kochat-master/demo/application.py
@@ -11,7 +11,7 @@ from kochat.loss import CRFLoss, CosFace, CenterLoss, COCOLoss, CrossEntropyLoss
 from kochat.model import intent, embed, entity
 from kochat.proc import DistanceClassifier, GensimEmbedder, EntityRecognizer, SoftmaxClassifier
 
-from demo.scenario import dust, weather, travel, restaurant
+from demo.scenario import dust, weather, travel, restaurant, school
 # from scenario import dust, weather, travel, restaurant
 # 에러 나면 이걸로 실행해보세요!
 
@@ -35,7 +35,7 @@ kochat = KochatApi(
     intent_classifier=(clf, True),
     entity_recognizer=(rcn, True),
     scenarios=[
-        weather, dust, travel, restaurant
+        weather, dust, travel, restaurant, school
     ]
 )
 
